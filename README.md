911a79707c70c2d874e62fce59e0f528098d8458
Chris Cundill <chrisc@diligentsoft.co>
2018-01-22 00:26:01 +0000

    REG-353 docker-compose working for all services and ui

--- README.md
+++ README.md
@@ -1,14 +1,40 @@
 # sbr superproject
 
 The purpose of this superproject is to simplify the process of working with
-the sbr projects.  Gitslave is used to aggregate git commands
-across the repositories while Docker compose is employed to run the services.
+the sbr projects.  The scripts provided take care of cloning the various git repos,
+running the appropriate build command against each and running the apps using Docker 
+compose.
 
-## Populate
+## Prerequisites
+
+You'll need the following tools installed:
+
+ * git
+ * scala
+ * sbt
+ * docker
 
 ## Build
 
+Simply run
+
+```bash
+./build.scala
+```
+
 ## Run
+
+A simple wrapper for `docker-compose` has been provided:
+
+```bash
+./run.sh
+```
+
+---
+---
+
+
+### Misc
 
 To remove all stopped containers, run:
 
@@ -22,7 +48,7 @@ To remove exited containers, run:
 docker rm $(docker ps -a -f status=exited -q)
 ```
 
-If you ever need to reset your Docker environment you can simply run:
+If you ever need to reset your whole Docker environment you can simply run:
 
 ```bash
 docker system prune -a
---
+++ build.scala
@@ -0,0 +1,75 @@
+#!/bin/sh
+exec scala "$0" "$@"
+!#
+import java.io.File
+
+import scala.sys.process._
+
+abstract class BaseProjectDef(val name: String,
+                              val localPath: String,
+                              val gitBaseUrl: String = "git@github.com:ONSdigital",
+                              val branch: Option[String],
+                              val buildCommand:  String)
+
+abstract class CategorisedProjectDef(val category: String,
+                                     override val name: String,
+                                     override val buildCommand: String,
+                                     override val branch: Option[String])
+  extends BaseProjectDef(name = name,
+    localPath = s"projects/$category/$name",
+    buildCommand = buildCommand,
+    branch = branch
+  )
+
+case class PluginProject(override val name: String, override val branch: Option[String] = None)
+  extends CategorisedProjectDef(category = "plugin", name = name, buildCommand = "sbt clean compile publishLocal", branch = branch)
+
+case class ApiProject(override val name: String, override val branch: Option[String] = None)
+  extends CategorisedProjectDef(category = "api", name = name, buildCommand = "sbt clean compile docker:publishLocal", branch = branch)
+
+case class UiProject(override val name: String, override val branch: Option[String] = None)
+  extends CategorisedProjectDef(category = "ui", name = name, buildCommand = "docker build -t sbr-ui .", branch = branch)
+
+object Projects {
+
+  private val definitions = List(
+    PluginProject("sbt-code-quality"),
+    PluginProject("sbt-scala-defaults"),
+    PluginProject("sbt-play-defaults"),
+    ApiProject("sbr-api", Some("feature/REG-362")),
+    ApiProject("sbr-control-api", Some("feature/REG-353")),
+    ApiProject("sbr-admin-data", Some("feature/REG-353")),
+    UiProject("sbr-ui", Some("feature/dockerfile"))
+  )
+
+  def build() {
+    gitUpdate()
+    buildProjects()
+  }
+
+  private def gitUpdate() {
+    println("Populating projects from git repos...")
+    definitions.foreach(d => {
+      val gitUrl = s"${d.gitBaseUrl}/${d.name}.git"
+      if (!new File(d.localPath).exists()) {
+        println(s"Cloning ${gitUrl}...")
+        (s"git clone ${gitUrl} ${d.localPath}" !)
+      }
+      if (d.branch.isDefined) {
+        println(s"Checking out branch ${d.branch.get} on repo ${gitUrl}...")
+        Process(s"git checkout ${d.branch.get}", new File(d.localPath)).!
+      }
+      s"cd ${d.localPath} && git pull" !
+    })
+  }
+
+  private def buildProjects() {
+    definitions.foreach(d => {
+      println(s"Building project ${d.name}...")
+      Process(d.buildCommand, new File(d.localPath)).!
+    })
+  }
+
+}
+
+Projects.build()
